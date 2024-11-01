61f1cf3ded30bc4aa8c4ce986d575096c6c52d3b
Fedegewis <Fedegewis@gmail.com>
2024-11-01 11:08:00 -0300

    cambios en cart, y creacion de cart service

--- TPO_Front_ecommerce_grupo4/src/pages/Cart.jsx
+++ TPO_Front_ecommerce_grupo4/src/pages/Cart.jsx
@@ -1,24 +1,52 @@
-import React from 'react';
-const onCleanCart = () => {
-    setAllProducts([]);
-    setTotal(0);
-    setCountProducts(0);
-};
+import React,{useEffect, useState} from 'react';
+import '../components/Cart.css'
+import productsData from '../productsCart.json' 
 
+const Cart = () => {
+    const [products, setProducts] = useState(productsData); 
+    const[total,setTotal]=useState(0)
+    const totalFinal=total.toFixed(2);
 
+    const handleRemoveProduct = (id) => {
+        
+        const updatedProducts = products.filter(product => product.id !== id);
+        setProducts(updatedProducts);
+        console.log(updatedProducts); 
+    };
+    useEffect(()=>{
+        const newTotal = products.reduce((acc, product) => acc + product.price, 0); 
+        setTotal(newTotal);
+    },[products]);
 
+    const handleCleanCart = () => {
+        setProducts([]); // Vacía el carrito
+    };
 
-const Cart = ({ total, onCleanCart }) => {
+    
     return (
-        <>
-            <h3>Total:</h3>
-            <span className='total-pagar'>${5000}</span>
-            <div>
-            <button className='btn-clear-all' onClick={onCleanCart}>
-                Vaciar Carrito
-            </button>
+        <div className="cart-container">
+            <h3 className="cart-title">Carrito de Compras</h3>
+            
+            <div className="products-container">
+                {products.map((product) => (
+                    <div className="product-item" key={product.id}>
+                        <h4 className="product-name">{product.name}</h4>
+                        <p className="product-price">Precio: ${product.price}</p>
+                        <button className='btn-remove' onClick={() => handleRemoveProduct(product.id)}>
+                            Eliminar
+                        </button>
+                    </div>
+                ))}
+                {products.length === 0 && <p>No hay productos en el carrito.</p>} 
             </div>
-        </>
+            <div className="total-container">
+            <span className='total-pagar'>Total: ${totalFinal}</span>
+                <button className='btn-clear-all' onClick={handleCleanCart}>
+                    Vaciar Carrito
+                </button>
+            </div>
+            
+        </div>
     );
 }
 
