58f5f315df558bbbee2f0ca987160ae5597e4233
Fedegewis <Fedegewis@gmail.com>
2024-11-01 11:57:08 -0300

    mejoras sobre el carrito con imagen

--- TPO_Front_ecommerce_grupo4/src/pages/Cart.jsx
+++ TPO_Front_ecommerce_grupo4/src/pages/Cart.jsx
@@ -1,51 +1,71 @@
-import React,{useEffect, useState} from 'react';
-import '../components/Cart.css'
-import productsData from '../productsCart.json' 
+import React, { useEffect, useState } from 'react';
+import '../components/Cart.css';
+import { getProductsCart } from '../services/cartService';
 
 const Cart = () => {
-    const [products, setProducts] = useState(productsData); 
-    const[total,setTotal]=useState(0)
-    const totalFinal=total.toFixed(2);
+    const [products, setProducts] = useState([]); 
+    const [total, setTotal] = useState(0);
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const initialProducts = await getProductsCart();
+                setProducts(initialProducts);
+            } catch (error) {
+                console.error("Error al cargar los productos:", error);
+                
+            }
+        };
+        fetchProducts();
+    }, []);
+
 
     const handleRemoveProduct = (id) => {
-        
         const updatedProducts = products.filter(product => product.id !== id);
         setProducts(updatedProducts);
-        console.log(updatedProducts); 
     };
-    useEffect(()=>{
-        const newTotal = products.reduce((acc, product) => acc + product.price, 0); 
+
+    useEffect(() => {
+        const newTotal = products.reduce((acc, product) => acc + product.price, 0);
         setTotal(newTotal);
-    },[products]);
+    }, [products]);
 
     const handleCleanCart = () => {
-        setProducts([]); // Vacía el carrito
+        setProducts([]); 
     };
 
-    
     return (
         <div className="cart-container">
             <h3 className="cart-title">Carrito de Compras</h3>
             
             <div className="products-container">
-                {products.map((product) => (
-                    <div className="product-item" key={product.id}>
-                        <h4 className="product-name">{product.name}</h4>
-                        <p className="product-price">Precio: ${product.price}</p>
-                        <button className='btn-remove' onClick={() => handleRemoveProduct(product.id)}>
-                            Eliminar
-                        </button>
-                    </div>
-                ))}
-                {products.length === 0 && <p>No hay productos en el carrito.</p>} 
+                {products.length > 0 ? (
+                    products.map((product) => (
+                        <div className="product-item" key={product.id}>
+                            {product.images.length>0 && (
+                                <img 
+                                src={product.images[0].imageBase64} 
+                                alt={product.name}
+                                className='produc-image'
+                                />
+                                )}     
+                            <h4 className="product-name">{product.name}</h4>
+                            <p className="product-price">Precio: ${product.price}</p>
+                            <button className='btn-remove' onClick={() => handleRemoveProduct(product.id)}>
+                                Eliminar
+                            </button>
+                        </div>
+                    ))
+                ) : (
+                    <p>No hay productos en el carrito.</p>
+                )}
             </div>
             <div className="total-container">
-            <span className='total-pagar'>Total: ${totalFinal}</span>
+                <span className='total-pagar'>Total: ${total.toFixed(2)}</span>
                 <button className='btn-clear-all' onClick={handleCleanCart}>
                     Vaciar Carrito
                 </button>
             </div>
-            
         </div>
     );
 }
--- TPO_Front_ecommerce_grupo4/src/services/cartService.js
+++ TPO_Front_ecommerce_grupo4/src/services/cartService.js
@@ -1,3 +1,12 @@
 import axios from "axios";
 
-export const getProductCart
+export const getProductsCart = async () => {
+    try{
+        const response =  await axios.get('/data/db.json');
+        return Array.isArray (response.data.products) ? response.data.products: [];
+    }catch(error){
+        console.error("Error al obtener los datos del producto del carrito ",error);
+        return [];
+    }
+    
+}
