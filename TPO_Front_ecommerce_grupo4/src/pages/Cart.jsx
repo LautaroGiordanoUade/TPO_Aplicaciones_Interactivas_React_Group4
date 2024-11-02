ab080f7fa80f17295afbe9e31962aa5e05c0fc7f
Fedegewis <80460038+Fedegewis@users.noreply.github.com>
2024-11-02 12:42:17 -0300

    Carrito compras (#4)
    * cambios en cart, y creacion de cart service
    
    * mejoras sobre el carrito con imagen
    
    * Cambios de carrito y creacion de checkout
    
    * Update Cart.css

--- TPO_Front_ecommerce_grupo4/src/pages/Cart.jsx
+++ TPO_Front_ecommerce_grupo4/src/pages/Cart.jsx
@@ -1,24 +1,84 @@
-import React from 'react';
-const onCleanCart = () => {
-    setAllProducts([]);
-    setTotal(0);
-    setCountProducts(0);
-};
+import React, { useEffect, useState } from 'react';
+import '../components/Cart.css';
+import { getProductsCart } from '../services/cartService';
+import { useNavigate } from 'react-router-dom';
+
+const Cart = () => {
+    const [products, setProducts] = useState([]); 
+    const [total, setTotal] = useState(0);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const fetchCartProducts = async () => {
+            try {
+                const initialCartProducts = await getProductsCart();
+                setProducts(initialCartProducts);
+            } catch (error) {
+                console.error("Error al cargar los productos:", error);
+                
+            }
+        };
+        fetchCartProducts();
+    }, []);
 
 
+    const handleRemoveCartProduct = (id) => {
+        const updatedProducts = products.filter(product => product.id !== id);
+        setProducts(updatedProducts);
+    };
 
+    useEffect(() => {
+        const newTotal = products.reduce((acc, product) => acc + product.price, 0);
+        setTotal(newTotal);
+    }, [products]);
 
-const Cart = ({ total, onCleanCart }) => {
+    const handleCleanCart = () => {
+        setProducts([]); 
+    };
+    
+
+    const handleCheckout = () => {
+        navigate('/checkout', { state: { items: products, total: total } });
+    };
+
     return (
-        <>
-            <h3>Total:</h3>
-            <span className='total-pagar'>${5000}</span>
-            <div>
-            <button className='btn-clear-all' onClick={onCleanCart}>
-                Vaciar Carrito
-            </button>
+        <div className="cart-container">
+            <h3 className="cart-title">Carrito de Compras</h3>
+            
+            <div className="products-container">
+                {products.length > 0 ? (
+                    products.map((product) => (
+                        <div className="product-item" key={product.id}>
+                            {product.images.length>0 && (
+                                <img 
+                                src={product.images[0].imageBase64} 
+                                alt={product.name}
+                                className='produc-image'
+                                />
+                                )}     
+                            <h4 className="product-name">{product.name}</h4>
+                            <p className="product-price">Precio: ${product.price}</p>
+                            <button className='btn-remove' onClick={() => handleRemoveCartProduct(product.id)}>
+                                Eliminar
+                            </button>
+                        </div>
+                    ))
+                ) : (
+                    <p>No hay productos en el carrito.</p>
+                )}
             </div>
-        </>
+            <div className="total-container">
+                <span className='total-pagar'>Total: ${total.toFixed(2)}</span>
+                <button className='btn-clear-all' onClick={handleCleanCart}>
+                    Vaciar Carrito
+                </button>
+            </div>
+            <div>
+            <button className='btn-checkout' onClick={handleCheckout}>
+                    Realizar Compra
+                </button>
+            </div>
+        </div>
     );
 }
 
--- TPO_Front_ecommerce_grupo4/src/routes/AppRoutes.jsx
+++ TPO_Front_ecommerce_grupo4/src/routes/AppRoutes.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home.jsx";
 import Header from "../components/Header.jsx";
 import Cart from "../pages/Cart.jsx";
+import Checkout from  "../pages/Checkout.jsx";
 import ProductDetail from '../pages/ProductDetail.jsx';
 
 const AppRoutes = () => {
@@ -12,6 +13,7 @@ const AppRoutes = () => {
                 <Routes>
                     <Route path="/" element={<Home/>} />
                     <Route path="/cart" element={<Cart/>} />
+                    <Route path="/checkout" element= {<Checkout/>}/>
                     <Route path="product/:id" element={<ProductDetail/>} />
                 </Routes>
             </div>
