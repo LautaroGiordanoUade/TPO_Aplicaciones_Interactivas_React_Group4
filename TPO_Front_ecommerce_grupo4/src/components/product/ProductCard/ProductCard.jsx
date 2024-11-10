0bf03f33018e98efe3a11aae71b9d0171ec4c2f1
frarenas <francisco.arenas@mercadolibre.com>
2024-11-10 17:07:39 -0300

    image placeholder

--- TPO_Front_ecommerce_grupo4/src/components/product/ProductCard/ProductCard.jsx
+++ TPO_Front_ecommerce_grupo4/src/components/product/ProductCard/ProductCard.jsx
@@ -6,10 +6,12 @@ import placeholderImage from '/public/placeholder.png';
 
 const ProductCard = ({ product }) => {
   const { images, name, price } = product;
+  console.log(images);
+  console.log(images.length);
   return (
     <Card className="card d-flex flex-column">
       <Link to={"/product/" + product.id}>
-        <Card.Img className="image" variant="top" src={images? images[0]?.imageBase64 : placeholderImage} />
+        <Card.Img className="image" variant="top" src={images && images.length > 0? images[0]?.imageBase64 : placeholderImage} />
         <Card.Body className="d-flex flex-column justify-content-end align-items-center">
           <Card.Title className="title">{name}</Card.Title>
           <Card.Text className="price">${price}</Card.Text>
