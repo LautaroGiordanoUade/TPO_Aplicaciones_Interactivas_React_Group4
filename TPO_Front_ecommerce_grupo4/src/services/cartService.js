26b77dad9a8a6dd91781ce24714d640d8db0af18
LautaroGiordanoUade <laugiordano@uade.edu.ar>
2024-11-12 09:59:12 -0300

    fix de purchase history, agregado el comprar dfinitivo y mas cosas de… (#14)
    * fix de purchase history, agregado el comprar dfinitivo y mas cosas de login
    
    * db json
    
    ---------
    
    Co-authored-by: lautarouade <lautarogiordanouade@gmail.com>

--- TPO_Front_ecommerce_grupo4/src/services/cartService.js
+++ TPO_Front_ecommerce_grupo4/src/services/cartService.js
@@ -16,10 +16,32 @@ export const getPurchaseHistory = async (user) => {
     const response = await apiClient.get('/purchase-history');
     
     // Filtrar las compras de acuerdo al userId
-    const filteredHistory = response.data.filter(purchase => purchase.userId === user.id);
+    const filteredHistory = response.data.filter(purchase => purchase.userId === user.userId);
     
     return filteredHistory;
   } catch (error) {
     console.error('Error intentando obtener purchase history: ' + error.message);
+  }
+};
+
+
+export const postPurchaseHistory = async (userId, items) => {
+  try {
+    const purchaseData = {
+      userId: userId,
+      date: new Date().toISOString(),
+      products: items.map((item) => ({
+        id: item.id,
+        name: item.name,
+        price: item.price,
+        quantity: item.quantityOnCart,
+      })),
+    };
+
+    const response = await apiClient.post('/purchase-history', purchaseData);
+    return response.data;
+  } catch (error) {
+    console.error('Error intentando comprar productos: ' + error.message);
+    throw error;
   }
 };
