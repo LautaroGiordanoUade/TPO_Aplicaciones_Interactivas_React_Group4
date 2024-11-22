0ff4341d41bfa7a00445c99b90134dcef84c8ddf
LautaroGiordanoUade <laugiordano@uade.edu.ar>
2024-11-22 18:17:29 -0300

    Revert "Te cruzo y te piso carrito de compras (#25)" (#26)
    This reverts commit 4d50a9a37df9fc568da03f8050a9a0b1d061da4e.

--- TPO_Front_ecommerce_grupo4/src/services/cartService.js
+++ TPO_Front_ecommerce_grupo4/src/services/cartService.js
@@ -1,48 +1,39 @@
+import axios from "axios";
 import apiClient from "./apiClient"
 
 export const getProductsCart = async () => {
-    const response = await apiClient.get('cart');
+    const response = await apiClient.get('cart-Products');
     return response.data    
 }
 
 export const deleteProductCart = async (id) => {
-  const response =  await apiClient.delete(`cart/remove/${id}`);
+  const response =  await apiClient.delete(`cart-Products/${id}`);
   return response.data;
 }
 
 
 export const deleteAllProductCart = async () => {
-  const response = await apiClient.delete('cart/empty');
+  const response = await apiClient.delete('cart-Products');
   return response.data;
 }
 
-export const createProductCart = async (productCart) => {
-  try{
-    const response =  await apiClient.post(`cart/add`,JSON.stringify(productCart));
-    return response.data;
-  }
-   catch (error) {
-    console.error('Error intentando crear el producto en el carrito: ' + error.message);
-  }
+export const createProductCart = async (product) => {
+  const response =  await apiClient.post('cart-Products', product);
+  return response.data;
 }
 
 export const updateProductCart = async (productCart) => {
-  try{
-    const response =  await apiClient.put(`cart/update`,  JSON.stringify(productCart)); 
-    return response.data;
-  }
-  catch (error) {
-    console.error('Error intentando actualizar el producto en el carrito: ' + error.message);
-  }
+  const response =  await apiClient.put(`cart-Products/${productCart.id}`,  JSON.stringify(productCart));
+  return response.data;
 }
 
 export const checkIfProductExistsInCart = async (productId) => {
       const cartProducts = await getProductsCart();
       return cartProducts.some(product => product.id === productId); // Retorna true si existe, false si no
   }
-  export const getProductQuantityInCart = async (productCart) => {
-    const response = await apiClient.get(`cart/quantity`.JSON.stringify(productCart));   
-    return response.data.quantity; 
+  export const getProductQuantityInCart = async (id) => {
+    const response = await apiClient.get(`cart-Products/${id}`);
+    return response.data.quantityOnCart; // Asegúrate de que el formato de la respuesta sea correcto
 };
  
 //TODO: cuando la integracion se realice , cambiar para q envie los datos de usuario y con eso ya filtra
@@ -58,7 +49,7 @@ export const getPurchaseHistory = async (user) => {
     console.error('Error intentando obtener purchase history: ' + error.message);
   }
 };
-// VER CON LAUTI
+
 
 export const postPurchaseHistory = async (userId, items) => {
   try {
@@ -69,7 +60,7 @@ export const postPurchaseHistory = async (userId, items) => {
         id: item.id,
         name: item.name,
         price: item.price,
-        quantity: item.quantity,
+        quantity: item.quantityOnCart,
       })),
     };
 
@@ -80,4 +71,3 @@ export const postPurchaseHistory = async (userId, items) => {
     throw error;
   }
 };
-//VER CON LAUTI
