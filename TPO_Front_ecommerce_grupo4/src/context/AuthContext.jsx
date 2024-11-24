969da7b86c9d6f5e8cc19957c232282c3c5c78cc
lautarouade <lautarogiordanouade@gmail.com>
2024-11-24 19:07:41 -0300

    expiration token added and flow too

--- TPO_Front_ecommerce_grupo4/src/context/AuthContext.jsx
+++ TPO_Front_ecommerce_grupo4/src/context/AuthContext.jsx
@@ -1,6 +1,8 @@
 import { createContext, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { logOutUser } from "../services/userService";
+import ToastMessage from "../components/ToastMessage";
+import LoadingSpinner from "../components/LoadingSpinner/LoadingSpinner"
 
 const AuthContext = createContext();
 
@@ -10,11 +12,14 @@ export function AuthProvider({ children }) {
     return JSON.parse(savedUser) ?? null;
   });
   const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [showToast, setShowToast] = useState(false); 
+  const [toastMessage, setToastMessage] = useState(""); 
   const navigate = useNavigate();
 
   const login = async (username, password, userId, role) => {
     try {
-      const response = {username, password, userId, role};
+      const response = { username, password, userId, role };
       setUser(response);
       navigate("/");
     } catch (err) {
@@ -22,18 +27,43 @@ export function AuthProvider({ children }) {
     }
   };
 
-  const logout = () => {
-    setUser(null);
-    logOutUser();
-    navigate("/userLogin");
+  const logout = async () => {
+    try {
+      setLoading(true);
+      setToastMessage("La sesión expiró, por favor vuelva a ingresar");
+      setShowToast(true);
+
+      setTimeout(() => {
+        setUser(null);
+        logOutUser();
+        navigate("/userLogin");
+        setLoading(false); 
+        setShowToast(false); 
+      }, 2000);
+    } catch (err) {
+      setError("Ocurrió un error al cerrar sesión");
+      setLoading(false);
   };
+};
 
   useEffect(() => {
     setError(null);
   }, [user]);
 
+  const propsLoading = {
+    text:'Re dirigiendo al loggin, por favor espere...'
+  }
+
   return (
     <AuthContext.Provider value={{ user, login, logout, error }}>
+      {loading && (<LoadingSpinner {...propsLoading} />)}
+      <ToastMessage
+        show={showToast}
+        setShow={setShowToast}
+        message={toastMessage}
+        variant="success"
+      />
+
       {children}
     </AuthContext.Provider>
   );
--- TPO_Front_ecommerce_grupo4/src/services/apiClient.js
+++ TPO_Front_ecommerce_grupo4/src/services/apiClient.js
@@ -1,7 +1,4 @@
 import axios from "axios";
-import { useNavigate } from 'react-router-dom';
-import { logOutUser } from "./userService";
-
 const baseUrlDev = 'http://localhost:3000/';
 const baseUrlProd = 'http://localhost:8080/api/v1/';
 
@@ -12,31 +9,27 @@ const apiClient = axios.create({
     }
 });
 
+
 apiClient.interceptors.request.use(
   (config) => {
-    const tokenData = JSON.parse(localStorage.getItem("token")); // Obtener el objeto del localStorage
-    const navigate = useNavigate();
+    const tokenData = JSON.parse(localStorage.getItem("token"))
+    const currentTime = Date.now(); 
 
     if (tokenData && tokenData.token) {
-
-      const decodedToken = JSON.parse(atob(tokenData.token.split('.')[1]));
-      const currentTime = Math.floor(Date.now() / 1000);
-
-      // Verifica si el token se vencio (mismo tiempo establecido en back)
-      if (decodedToken.exp < currentTime) {
-        logOutUser()
-        navigate("/login");
+      const expirationTime = tokenData.expiration;
+      if (currentTime > expirationTime) {
         return Promise.reject("Token expired");
       }
-
-      // Si el token es válido, añadirlo al encabezado Authorization
-      config.headers.Authorization = `Bearer ${tokenData.token}`;
     } else {
-      // Si no hay token, redirigir al login
-      navigate("/login"); // Redirigir al login si no hay token
+      return Promise.reject("No token found");
+    }
+
+    if (tokenData && tokenData.token) {
+      config.headers.Authorization = `Bearer ${tokenData.token}`;
     }
 
     return config;
+
   },
   (error) => {
     return Promise.reject(error);
