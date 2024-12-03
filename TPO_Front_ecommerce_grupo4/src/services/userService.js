d6d9e2fcd6b3efd1b95239726cc8449a1740dab8
lautarouade <lautarogiordanouade@gmail.com>
2024-12-03 18:53:08 -0300

    reverted time expired

--- TPO_Front_ecommerce_grupo4/src/services/userService.js
+++ TPO_Front_ecommerce_grupo4/src/services/userService.js
@@ -4,7 +4,7 @@ import apiClientPublic from "./apiClientPublic";
 export const loginUser = async (email, password) => {
   const response = await apiClientPublic.post('auth/authenticate', { email, password });
   const userData = response.data;
-  const tokenTimer = 0.5;
+  const tokenTimer = 10;
 
   if (userData?.access_token) {
     const expirationTime = new Date().getTime() + tokenTimer* 60 * 1000; // Tiempo actual + tokenTimer minutos
